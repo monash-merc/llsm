993697851f069308c5488cdcb87a45107a9de1dc
Keith Schulze <keith.schulze@gmail.com>
2016-10-03 14:49:32 +1100

    Improvements to the ImageJ deskew plugins... voxel sizes, multi-channels and specification of deskew paramaters

--- ij/src/main/scala/llsm/ij/DeskewPlugin.scala
+++ ij/src/main/scala/llsm/ij/DeskewPlugin.scala
@@ -2,10 +2,10 @@ package llsm
 package ij
 
 import llsm.Deskew
-
+import java.lang.Math
 import net.imagej.Dataset
-import net.imagej.ops.OpService
-import net.imglib2.RandomAccessible
+import net.imagej.axis.{Axes, CalibratedAxis, DefaultLinearAxis}
+import net.imagej.DatasetService
 import net.imglib2.img.Img
 import net.imglib2.`type`.numeric.RealType
 import org.scijava.ItemIO
@@ -18,11 +18,17 @@ class DeskewPlugin extends Command {
   @Parameter(`type` = ItemIO.INPUT)
   var input: Dataset = _
 
+  @Parameter(label = "X/Y voxel size (um)", required = true)
+  var pixelSize: Double = 0.1018
+
+  @Parameter(label = "Sample piezo increment", required = true)
+  var sampleIncrement: Double = 0.3000
+
   @Parameter
-  var ops: OpService = _
+  var ds: DatasetService = _
 
   @Parameter(`type` = ItemIO.OUTPUT)
-  var output: RandomAccessible[_] = _
+  var output: Dataset = _
 
   /**
   * Entry point to running a plugin.
@@ -30,6 +36,29 @@ class DeskewPlugin extends Command {
   override def run(): Unit = {
     val ip: Img[T] forSome {type T <: RealType[T]} = input.getImgPlus.getImg.asInstanceOf[Img[T] forSome {type T <: RealType[T]}]
 
-    output = Deskew.deskewStack(ip, 0, 2, 3)
+    val shearFactor: Int = Math.round(sampleIncrement / pixelSize).toInt
+
+    val zInterval: Double = Math.sin(Math.toRadians(31.8)) * sampleIncrement
+
+    var axes: Array[CalibratedAxis] = Array.ofDim[CalibratedAxis](input.numDimensions)
+
+    input.axes(axes)
+
+    axes = axes.map(a => {
+      val axisType = a.`type`()
+      if (axisType == Axes.X)
+        new DefaultLinearAxis(Axes.X, "um", pixelSize)
+      else if (axisType == Axes.Y)
+        new DefaultLinearAxis(Axes.Y, "um", pixelSize)
+      else if (axisType == Axes.Z)
+        new DefaultLinearAxis(Axes.Z, "um", zInterval)
+      else a
+    })
+
+    val xIndex = input.dimensionIndex(Axes.X)
+    val zIndex = input.dimensionIndex(Axes.Z)
+
+    output = ds.create(Deskew.deskewStack(ip, xIndex, zIndex, shearFactor))
+    output.setAxes(axes)
   }
 }
--- ij/src/main/scala/llsm/ij/RealDeskewPlugin.scala
+++ ij/src/main/scala/llsm/ij/RealDeskewPlugin.scala
@@ -2,11 +2,12 @@ package llsm
 package ij
 
 import llsm.Deskew
-
+import java.lang.Math
 import net.imagej.Dataset
-import net.imagej.ops.OpService
-import net.imglib2.RandomAccessible
+import net.imagej.axis.{Axes, CalibratedAxis, DefaultLinearAxis}
+import net.imagej.DatasetService
 import net.imglib2.img.Img
+import net.imglib2.interpolation.randomaccess.{NearestNeighborInterpolatorFactory, NLinearInterpolatorFactory, LanczosInterpolatorFactory}
 import net.imglib2.`type`.numeric.RealType
 import org.scijava.ItemIO
 import org.scijava.command.Command
@@ -18,11 +19,20 @@ class RealDeskewPlugin extends Command {
   @Parameter(`type` = ItemIO.INPUT)
   var input: Dataset = _
 
+  @Parameter(label = "X/Y voxel size (um)", required = true)
+  var pixelSize: Double = 0.1018
+
+  @Parameter(label = "Sample piezo increment", required = true)
+  var sampleIncrement: Double = 0.3000
+
+  @Parameter(label = "Interpolation scheme", choices = Array("Nearest Neighbour", "Linear", "Lanczos"), required = false)
+  var interpolation: String = "Nearest Neighbour"
+
   @Parameter
-  var ops: OpService = _
+  var ds: DatasetService = _
 
   @Parameter(`type` = ItemIO.OUTPUT)
-  var output: RandomAccessible[_] = _
+  var output: Dataset = _
 
   /**
   * Entry point to running a plugin.
@@ -30,6 +40,33 @@ class RealDeskewPlugin extends Command {
   override def run(): Unit =  {
     val ip: Img[T] forSome {type T <: RealType[T]} = input.getImgPlus.getImg.asInstanceOf[Img[T] forSome {type T <: RealType[T]}]
 
-    output = Deskew.deskewRealStack(ip, 0, 2, 2.94)
+    val shearFactor: Double = sampleIncrement / pixelSize
+
+    val zInterval: Double = Math.sin(Math.toRadians(31.8)) * sampleIncrement
+
+    var axes: Array[CalibratedAxis] = Array.ofDim[CalibratedAxis](input.numDimensions)
+
+    input.axes(axes)
+
+    axes = axes.map(a => {
+      val axisType = a.`type`()
+      if (axisType == Axes.X)
+        new DefaultLinearAxis(Axes.X, "um", pixelSize)
+      else if (axisType == Axes.Y)
+        new DefaultLinearAxis(Axes.Y, "um", pixelSize)
+      else if (axisType == Axes.Z)
+        new DefaultLinearAxis(Axes.Z, "um", zInterval)
+      else a
+    })
+
+    val xIndex = input.dimensionIndex(Axes.X)
+    val zIndex = input.dimensionIndex(Axes.Z)
+
+    output = interpolation match {
+      case "Nearest Neighbour" => ds.create(Deskew.deskewRealStack(ip, xIndex, zIndex, shearFactor, new NearestNeighborInterpolatorFactory()))
+      case "Linear" => ds.create(Deskew.deskewRealStack(ip, xIndex, zIndex, shearFactor, new NLinearInterpolatorFactory()))
+      case "Lanczos" => ds.create(Deskew.deskewRealStack(ip, xIndex, zIndex, shearFactor, new LanczosInterpolatorFactory()))
+    }
+    output.setAxes(axes)
   }
 }
