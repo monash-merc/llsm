660337f030ec75d0ae0fc98a653ba0193f97ca9b
Keith Schulze <keith.schulze@gmail.com>
2017-08-24 16:53:44 +1000

    Fix warts in tests sub project

--- tests/src/main/scala/llsm/io/metadata/MetadataSuite.scala
+++ tests/src/main/scala/llsm/io/metadata/MetadataSuite.scala
@@ -9,15 +9,19 @@ import cats.data.Kleisli
 import llsm.{BaseSuite, NoInterpolation}
 import llsm.algebras.{MetadataF, MetadataLow, MetadataLowF}
 import llsm.interpreters._
-import _root_.io.scif.SCIFIO
-
-import scala.util.{Failure, Success, Try}
 
 class MetadataSuite extends BaseSuite with MetadataImplicits {
 
   type Exec[M[_], A] = Kleisli[M, Unit, A]
 
-  private def metaLowMockCompiler[M[_]](s: Int, c: Int, t: Int)(implicit M: ApplicativeError[M, Throwable]) = λ[FunctionK[MetadataLowF, Exec[M, ?]]] {
+  private def metaLowMockCompiler[M[_]](
+    s: Int,
+    c: Int,
+    t: Int
+  )(
+    implicit
+    M: ApplicativeError[M, Throwable]
+  ): FunctionK[MetadataLowF, Exec[M, ?]] = λ[FunctionK[MetadataLowF, Exec[M, ?]]] {
     fa => Kleisli { _ =>
       fa match {
         case MetadataLow.ConfigurableMeta => M.pure(ConfigurableMetadata(0.1018, 0.1018, NoInterpolation))
@@ -37,18 +41,6 @@ class MetadataSuite extends BaseSuite with MetadataImplicits {
             case Left(e) => M.raiseError(e)
           }
         }
-        case MetadataLow.ExtractBasicImageMeta(path) => Try {
-          val scifio = new SCIFIO
-          val format = scifio.format().getFormat(path.toString)
-          format.createParser().parse(path.toString)
-        } match {
-          case Success(meta) => {
-            val im = meta.get(0)
-            val cals = Array[Double](0.1018, 0.1018, 1)
-            M.pure(Some(ImgMetadata(im.getAxesLengths, cals)))
-          }
-          case Failure(e) => M.raiseError(e)
-        }
         case MetadataLow.WriteMetadata(path, meta) => ???
       }
     }
@@ -57,7 +49,6 @@ class MetadataSuite extends BaseSuite with MetadataImplicits {
   def metaMockCompiler[M[_]](s: Int, c: Int, t: Int)(implicit M: ApplicativeError[M, Throwable]): MetadataF ~> M =
     new (MetadataF ~> M) {
       def apply[A](fa: MetadataF[A]): M[A] =
-        metaFToMetaLowF(fa).foldMap(metaLowMockCompiler[M](s, c, t)(M)).run(())
-
+        metaFToMetaLowF(fa).foldMap[Exec[M, ?]](metaLowMockCompiler[M](s, c, t)(M)).run(())
     }
 }
--- tests/src/test/scala/llsm/tests/io/MetadataAPISpec.scala
+++ tests/src/test/scala/llsm/tests/io/MetadataAPISpec.scala
@@ -37,13 +37,7 @@ class MetadataAPISpec extends MetadataSuite {
         Metadata[M].readMetadata(path)
 
       program[MetadataF](Paths.get(imgPath)).foldMap(metaMockCompiler[Try](channel*time, channel, time)) match {
-        case Success(FileMetadata(FilenameMetadata(_, _, st, ch, _, _, _), wave, cam, _, _, img)) => {
-          img match {
-            case Some(ImgMetadata(dims, _)) => {
-              dims should equal (Array(w.toLong, h.toLong, z.toLong, c.toLong, t.toLong).filter(_ > 1L))
-            }
-            case None => fail("Failed to read Image metadata")
-          }
+        case Success(FileMetadata(FilenameMetadata(_, _, st, ch, _, _, _), wave, cam, _, _)) => {
           st should equal (channel * time)
           ch should equal (channel)
         }
